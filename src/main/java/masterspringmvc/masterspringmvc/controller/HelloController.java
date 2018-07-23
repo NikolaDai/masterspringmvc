cd8eb145b6f8071738fec30e49f646e855f84322
NicolaDai <335860828@qq.com>
2018-07-23 23:09:14 +0800

    finish 2.9

--- src/main/java/masterspringmvc/masterspringmvc/controller/HelloController.java
+++ src/main/java/masterspringmvc/masterspringmvc/controller/HelloController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Collection;
 //import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -12,6 +15,13 @@ public class HelloController {
     //@ResponseBody
     public String hello(@RequestParam(defaultValue = "world") String name, Model model){
         //return "hello world";
+
+        Collection<String> c = new ArrayList<>();
+        c.add("hello");
+        c.add("world");
+        c.add("java");
+
+        model.addAttribute("tweets", c);
         model.addAttribute("message", "Hello " + name);
         return "resultPage";
     }
