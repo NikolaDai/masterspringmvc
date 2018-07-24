08b52feb794f0f5c237791234462a1f794ab1e5c
NicolaDai <335860828@qq.com>
2018-07-24 15:38:37 +0800

    finish tweeter test of 2.7.3

--- src/main/java/masterspringmvc/masterspringmvc/MasterspringmvcApplication.java
+++ src/main/java/masterspringmvc/masterspringmvc/MasterspringmvcApplication.java
@@ -2,10 +2,11 @@ package masterspringmvc.masterspringmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"org.springframework.social.twitter.api"})
 public class MasterspringmvcApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MasterspringmvcApplication.class, args);
     }
